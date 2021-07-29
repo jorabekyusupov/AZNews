1412998205739660369d93f584d8dc3b11fadef6
jorabekyusupov <jonistar31@gmail.com>
2021-07-29 17:53:47 +0500

    second

--- assets/php/connect.php
+++ assets/php/connect.php
@@ -3,7 +3,7 @@
         public $__connect;
         public function __construct()
         {
-            $this->__connect = new mysqli('localhost', 'root', 'root', 'aznews');
+            $this->__connect = new mysqli('aznewsa.mysql.database.azure.com', 'YUSUPOV@aznewsa', '8118787Aa', 'aznew');
         }
         function getConnect()
         {
