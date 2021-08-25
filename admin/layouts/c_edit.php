43b71ec6f54f817ff4790adbd8d16159a2577619
jorabekyusupov <jonistar31@gmail.com>
2021-08-25 10:54:47 +0500

    SSS

--- admin/layouts/c_edit.php
+++ admin/layouts/c_edit.php
@@ -14,7 +14,7 @@
             </div>
             <div  class="card-body">
                 <div hidden class="form-group row mb-4">
-                    <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">id</label>
+                    <label  class="col-form-label text-md-right col-12 col-md-3 col-lg-3">id</label>
                     <div class="col-sm-12 col-md-7">
                         <input type="text" class="form-control" name="id" value="<?= $c_items->id ?>">
                     </div>
